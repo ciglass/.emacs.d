122f87815de92286e07669405f1914671cb4355e
ciglass <cameron.i.glass@gmail.com>
2023-08-15 23:14:08 -0400

    Added ports.el

---
+++ sv_misc/ports.el
@@ -0,0 +1,25 @@
+(defun sv-port-toggle ()
+  "Toggle a System Verilog port on the current line between being an input and output."
+  ;;
+  (interactive)
+  (save-excursion
+    (let ((this-line (thing-at-point 'line t))
+          (leading-text)
+          (port-type)
+          (new-port-type)
+          (trailing-text))
+      (let ((m (string-match "\\(.*\\)\\(input\\|output\\)\\(.*\\)" this-line)))
+        (when m
+          (setq leading-text (match-string 1 this-line)
+                port-type (match-string 2 this-line)
+                trailing-text (match-string 3 this-line))
+          (cond
+           ((string-equal port-type "output")
+            (setq new-port-type "input"))
+           ((string-equal port-type "input")
+            (setq new-port-type "output")))
+          (kill-whole-line)
+          (insert (format "%s%s%s\n" leading-text new-port-type trailing-text))
+          ;; I find this convenient, but it may get slow for big files
+          ;; This is to help get around the fact that output and input have a different number of characters
+          (electric-verilog-tab))))))
