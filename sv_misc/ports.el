4936d502f384ffa575dc4c0e71435576e04a2ff2
ciglass <cameron.i.glass@gmail.com>
2023-08-18 00:50:41 -0400

    Added sv-port-toggle-region and refactored sv-port-toggle into a helper function

--- sv_misc/ports.el
+++ sv_misc/ports.el
@@ -1,13 +1,12 @@
-(defun sv-port-toggle ()
-  "Toggle a System Verilog port on the current line between being an input and output."
-  ;;
-  (interactive)
+(defun sv-port-toggle-helper ()
+  "Toggle a System Verilog port on the current line between being an input and input."
   (save-excursion
     (let ((this-line (thing-at-point 'line t))
           (leading-text)
           (port-type)
           (new-port-type)
           (trailing-text))
+      (message (format "Looking at line %s" this-line))
       (let ((m (string-match "\\(.*\\)\\(input\\|output\\)\\(.*\\)" this-line)))
         (when m
           (setq leading-text (match-string 1 this-line)
@@ -19,7 +18,31 @@
            ((string-equal port-type "input")
             (setq new-port-type "output")))
           (kill-whole-line)
-          (insert (format "%s%s%s\n" leading-text new-port-type trailing-text))
-          ;; I find this convenient, but it may get slow for big files
-          ;; This is to help get around the fact that output and input have a different number of characters
-          (electric-verilog-tab))))))
+          (insert (format "%s%s%s\n" leading-text new-port-type trailing-text)))))))
+
+(defun sv-port-toggle-line ()
+  "Toggle a System Verilog port on the current line between being an input and output."
+  ;;
+  (interactive)
+  (sv-port-toggle-helper)
+  ;; I find this convenient, but it may get slow for big files
+  ;; This is to help get around the fact that output and input have a different number of characters
+  (electric-verilog-tab))
+
+(defun sv-port-toggle-region (begin end)
+  "Toggle all the system verilog ports on the lines in the region between output and input.
+If any part of a line is contained within the region, that line's port direction will be toggled.
+
+Note: The behavior is inconsistent when the end of the region is the beginning of a line.
+Sometimes the line is toggled, sometimes it isn't. It doesn't seem worth it to fix since the workaround is easy."
+  (interactive "r")
+  (message (format "Toggling region between %d and %d" begin end))
+  (save-excursion
+    (goto-char begin)
+    (while (<= (point) end)
+      (sv-port-toggle-helper)
+      (next-line)
+      (beginning-of-line))
+    ;; I find this convenient, but it may get slow for big files
+    ;; This is to help get around the fact that output and input have a different number of characters
+    (electric-verilog-tab)))
