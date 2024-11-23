b80fa148092b3cd41ff14619e99694ddd8397dc8
ciglass <cameron.i.glass@gmail.com>
2024-11-23 13:00:59 -0500

    Added option for onehot encoding, added additional example in .v, reformatted enum_gen.py with black

--- sv_misc/enum_gen.py
+++ sv_misc/enum_gen.py
@@ -6,15 +6,14 @@ import argparse
 import math
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--enum",
-                    type=str)
-parser.add_argument("--values",
-                    type=str,
-                    nargs="+")
-parser.add_argument("--indent",
-                    type=int,
-                    default=2,
-                    required=False)
+parser.add_argument("--enum", type=str)
+
+parser.add_argument("--values", type=str, nargs="+")
+
+parser.add_argument("--indent", type=int, default=2, required=False)
+
+parser.add_argument("--onehot", action="store_true", default=False)
+
 args = parser.parse_args()
 
 outer_indent = " " * args.indent
@@ -26,15 +25,28 @@ for value in args.values:
         longest_value = len(value)
 
 enum_width = math.ceil(math.log2(len(args.values)))
+if args.onehot:
+    enum_width = len(args.values)
+
 enum_msb = enum_width - 1
 
 ret_arr = []
-ret_arr.append(f'{outer_indent}typedef enum logic [{enum_msb}:0] {{')
+ret_arr.append(f"{outer_indent}typedef enum logic [{enum_msb}:0] {{")
 for index, value in enumerate(args.values):
+
+    # Add spaces for extra padding to make the = character line up on each line
     space_padding = " " * (longest_value - len(value))
+
+    # Add trailing comma for all but the last value
     comma = ""
-    if (index != len(args.values) - 1):
+    if index != len(args.values) - 1:
         comma = ","
-    ret_arr.append(f'{inner_indent}{value}{space_padding} = {enum_width}\'d{index}{comma}')
-ret_arr.append(f'{outer_indent}}} {args.enum};')
-print('\n'.join(ret_arr))
+
+    # Render encoded or onehot values
+    rendered_value = f"{enum_width}'d{index}"
+    if args.onehot:
+        rendered_value = f"{enum_width}'d{1 << index}"
+    ret_arr.append(f"{inner_indent}{value}{space_padding} = {rendered_value}{comma}")
+
+ret_arr.append(f"{outer_indent}}} {args.enum};")
+print("\n".join(ret_arr))
--- sv_misc/py_insert_test.v
+++ sv_misc/py_insert_test.v
@@ -3,7 +3,7 @@ module py_insert_test
 
   );
 
-  /*AUTOINSERTLISP(insert-from-shell "python" "enum_gen.py" "--enum" "my_name_t" "--values" "FOO" "BAR" "BAZ" "QUUX" "Z")*/
+  /*AUTOINSERTLISP(insert-from-shell "python" "enum_gen.py" "--enum" "encoded_t" "--values" "FOO" "BAR" "BAZ" "QUUX" "Z")*/
   // Beginning of automatic insert lisp
   typedef enum logic [2:0] {
     FOO  = 3'd0,
@@ -11,7 +11,17 @@ module py_insert_test
     BAZ  = 3'd2,
     QUUX = 3'd3,
     Z    = 3'd4
-  } my_name_t;
+  } encoded_t;
+  // End of automatics
+
+  /*AUTOINSERTLISP(insert-from-shell "python" "enum_gen.py" "--enum" "onehot_t" "--onehot" "--values" "IDLE" "FLUSH" "WRITEBACK" "FINISH")*/
+  // Beginning of automatic insert lisp
+  typedef enum logic [3:0] {
+    IDLE      = 4'd1,
+    FLUSH     = 4'd2,
+    WRITEBACK = 4'd4,
+    FINISH    = 4'd8
+  } onehot_t;
   // End of automatics
 
 endmodule
